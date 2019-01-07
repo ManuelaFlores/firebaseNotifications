24686c92f287bcc03c1cdc5c9f8f25b62cb5fa56
Manuela Flores <fmanuela499@gmail.com>
2019-01-07 14:11:27 -0500

    feat(notifications): CLOSED :art:

--- app/src/main/java/com/manuflowers/notificationsfirebase/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/manuflowers/notificationsfirebase/MyFirebaseMessagingService.kt
@@ -1,11 +1,16 @@
 package com.manuflowers.notificationsfirebase
 
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.media.RingtoneManager
+import android.support.v4.app.NotificationCompat
+
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-class MyFirebaseMessagingService: FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
         sendNotification(remoteMessage.notification!!.body)
@@ -14,7 +19,19 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
     private fun sendNotification(body: String?) {
         var intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-
         var pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        var defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+      // NotificationCompat.Builder =
+        var notificationBuilder : NotificationCompat.Builder = NotificationCompat.Builder(this)
+        notificationBuilder.setSmallIcon(R.drawable.ic_stat_name)
+        notificationBuilder.setContentTitle("Android")
+        notificationBuilder.setContentText(body)
+        notificationBuilder.setAutoCancel(true)
+        notificationBuilder.setSound(defaultSoundUri)
+        notificationBuilder.setContentIntent(pendingIntent)
+
+        var notificationManager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
     }
 }
